dee848cbff3d369a85013eb4faeddba7feeda96e
Adam <holtsoftware@outlook.com>
2013-06-12 22:38:46 -0600

    Added in app voice command. Added color drop down

--- Relay/Windows Phone/Sannel.Relay/ControlPage.xaml.cs
+++ Relay/Windows Phone/Sannel.Relay/ControlPage.xaml.cs
@@ -11,20 +11,25 @@ using Windows.Networking.Sockets;
 using Sannel.Relay.ViewModels;
 using Sannel.Relay.Command;
 using System.IO;
+using Windows.Phone.Speech.Recognition;
+using System.Windows.Media;
 
 namespace Sannel.Relay
 {
 	public partial class ControlPage : PhoneApplicationPage
 	{
 		public static StreamSocket Connection = null;
-
+		private SpeechRecognizerUI speachUI = new SpeechRecognizerUI();
 		private CommandViewModel commandvm;
 		private Processer processer;
+		private List<ColorViewModel> colors = new List<ColorViewModel>();
 
-		
 		public ControlPage()
 		{
 			InitializeComponent();
+			speachUI.Settings.ExampleText = "On/Off";
+			speachUI.Settings.ListenText = "On or Off?";
+			speachUI.Recognizer.Grammars.AddGrammarFromList("lights", new String[] { "on", "off" });
 		}
 
 		protected async override void OnNavigatedTo(NavigationEventArgs e)
@@ -39,52 +44,121 @@ namespace Sannel.Relay
 				else
 				{
 					NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
-					
+
 				}
 				return;
 			}
 
-			processer = new Processer();
-			commandvm = new CommandViewModel();
-			commandvm.Relay = new RelayViewModel();
-			processer.CommandRecived += commandvm.Relay.OnCommandRecived;
-			commandvm.RGBLed = new RGBLedViewModel();
-			processer.CommandRecived += commandvm.RGBLed.OnCommandRecived;
-			commandvm.Relay.SendCommand += sendCommand;
-			commandvm.RGBLed.SendCommand += sendCommand;
-			DataContext = commandvm;
-			processer.SetInputStream(Connection.InputStream.AsStreamForRead());
-			processer.SetOutputStream(Connection.OutputStream.AsStreamForWrite());
-
-			if (NavigationContext.QueryString != null && NavigationContext.QueryString.ContainsKey("commandName"))
+			if (processer == null || commandvm == null || commandvm.RGBLed == null)
 			{
-				if (String.Compare("LightsOn", NavigationContext.QueryString["commandName"], StringComparison.CurrentCulture) == 0)
+				processer = new Processer();
+				commandvm = new CommandViewModel();
+				commandvm.Relay = new RelayViewModel();
+				processer.CommandRecived += commandvm.Relay.OnCommandRecived;
+				commandvm.RGBLed = new RGBLedViewModel();
+				processer.CommandRecived += commandvm.RGBLed.OnCommandRecived;
+				commandvm.Relay.SendCommand += sendCommand;
+				commandvm.RGBLed.SendCommand += sendCommand;
+				DataContext = commandvm;
+				processer.SetInputStream(Connection.InputStream.AsStreamForRead());
+				processer.SetOutputStream(Connection.OutputStream.AsStreamForWrite());
+
+				if (NavigationContext.QueryString != null && NavigationContext.QueryString.ContainsKey("commandName"))
 				{
-					commandvm.Relay.Relay1 = commandvm.Relay.Relay2 = commandvm.Relay.Relay3 = commandvm.Relay.Relay4 = true;
+					if (String.Compare("LightsOn", NavigationContext.QueryString["commandName"], StringComparison.CurrentCulture) == 0)
+					{
+						commandvm.Relay.Relay1 = commandvm.Relay.Relay2 = commandvm.Relay.Relay3 = commandvm.Relay.Relay4 = true;
+					}
+					else if (String.Compare("LightsOff", NavigationContext.QueryString["commandName"], StringComparison.CurrentCulture) == 0)
+					{
+						commandvm.Relay.Relay1 = commandvm.Relay.Relay2 = commandvm.Relay.Relay3 = commandvm.Relay.Relay4 = false;
+					}
 				}
-				else if (String.Compare("LightsOff", NavigationContext.QueryString["commandName"], StringComparison.CurrentCulture) == 0)
+
+				CommandArgs args = new CommandArgs();
+				args.Type = CommandType.Get;
+				args.Command = Command.Command.All;
+				args.Value = new byte[0];
+				
+				colors.AddRange(new ColorViewModel[]
 				{
-					commandvm.Relay.Relay1 = commandvm.Relay.Relay2 = commandvm.Relay.Relay3 = commandvm.Relay.Relay4 = false;
-				}	
-			}
+					new ColorViewModel
+					{
+						Name = "None",
+						Color = Colors.Black
+					},
+					new ColorViewModel()
+					{
+						Name = "Blue",
+						Color = Colors.Blue
+					},
+					new ColorViewModel
+					{
+						Name = "Brown",
+						Color = Colors.Brown
+					},
+					new ColorViewModel{
+						Name = "Cyan",
+						Color = Colors.Cyan
+					},
+					new ColorViewModel
+					{
+						Name = "DarkGray",
+						Color = Colors.DarkGray
+					},
+					new ColorViewModel
+					{
+						Name = "Gray",
+						Color = Colors.Gray
+					},
+					new ColorViewModel
+					{
+						Name = "Green",
+						Color = Colors.Green
+					},
+					new ColorViewModel
+					{
+						Name = "LightGray",
+						Color = Colors.LightGray
+					},
+					new ColorViewModel
+					{
+						Name = "Magenta",
+						Color = Colors.Magenta
+					},
+					new ColorViewModel
+					{
+						Name = "Orange",
+						Color = Colors.Orange
+					},
+					new ColorViewModel
+					{
+						Name = "Purple",
+						Color = Colors.Purple
+					},
+					new ColorViewModel
+					{
+						Name = "Red",
+						Color = Colors.Red
+					},
+					new ColorViewModel
+					{
+						Name = "White",
+						Color = Colors.White
+					},
+					new ColorViewModel
+					{
+						Name = "Yellow",
+						Color = Colors.Yellow
+					}
+				});
+				ColorsInput.ItemsSource = colors;
 
-			CommandArgs args = new CommandArgs();
-			args.Type = CommandType.Get;
-			args.Command = Command.Command.All;
-			args.Value = new byte[0];
-			if (processer != null)
-			{
-				await processer.SendCommandAsync(args);
+				if (processer != null)
+				{
+					await processer.SendCommandAsync(args);
+				}
 			}
-			/*processer = new CommandProcesser(Connection);
-			vm = new ControlViewModel();
-			DataContext = vm;
-			vm.Relay = new RelayViewModel();
-			processer.AddCommandClass(vm.Relay);
-			vm.LED = new RGBLedViewModel();
-			processer.AddCommandClass(vm.LED);
-			processer.SendGetAll();*/
-
 		}
 
 		private async void sendCommand(object sender, CommandArgs args)
@@ -92,6 +166,33 @@ namespace Sannel.Relay
 			if (processer != null)
 			{
 				await processer.SendCommandAsync(args);
+			}
+		}
+
+		protected async void Speech_Click(object sender, EventArgs e)
+		{
+			var results = await speachUI.RecognizeWithUIAsync();
+			if (results.ResultStatus == SpeechRecognitionUIStatus.Succeeded)
+			{
+				if (String.Compare(results.RecognitionResult.Text, "on") == 0)
+				{
+					commandvm.Relay.Relay1 = commandvm.Relay.Relay2 = commandvm.Relay.Relay3 = commandvm.Relay.Relay4 = true;
+				}
+				else if (String.Compare(results.RecognitionResult.Text, "off") == 0)
+				{
+					commandvm.Relay.Relay1 = commandvm.Relay.Relay2 = commandvm.Relay.Relay3 = commandvm.Relay.Relay4 = false;
+				}
+			}
+		}
+
+		private void ColorsInput_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (e.AddedItems.Count > 0)
+			{
+				ColorViewModel color = e.AddedItems[0] as ColorViewModel;
+				commandvm.RGBLed.Red = color.Color.R;
+				commandvm.RGBLed.Green = color.Color.G;
+				commandvm.RGBLed.Blue = color.Color.B;
 			}
 		}
 	}
---
+++ Relay/Windows Phone/Sannel.Relay/ViewModels/ColorViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Sannel.Relay.ViewModels
+{
+	public class ColorViewModel : Sannel.ComponentModel.NotifyPropertyChangedBase
+	{
+		private String name;
+		public String Name
+		{
+			get
+			{
+				return name;
+			}
+			set
+			{
+				SetProperty(ref name, value, "Name");
+			}
+		}
+
+		private Color color;
+		public Color Color
+		{
+			get
+			{
+				return color;
+			}
+			set
+			{
+				SetProperty(ref color, value, "Color");
+			}
+		}
+	}
+}
